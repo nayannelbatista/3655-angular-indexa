65d9612087e4869a41acc55d78542701c79f34bd
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-02-05 15:18:18 -0300

    Vídeo 1.3 - utilizando HttpClient

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,33 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Contato } from '../componentes/contato/contato';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContatoService {
 
-  private contatos: Contato[] = [
-    {"id": 1, "nome": "Ana", "telefone": "29 278869420", "email": "email@emal.com"},
-  ]
+  private readonly API = 'http://localhost:3000/contatos';
 
-  constructor() {
-    //Tentar obter os dados do localStorage
-    const contatosLocalStorageString = localStorage.getItem('contatos');
-    const contatosLocalStorage =
-      contatosLocalStorageString ? JSON.parse(contatosLocalStorageString) : null;
-
-    this.contatos = contatosLocalStorage || null;
-
-    //Salvar os contatos no localStorage
-    localStorage.setItem('contatos', JSON.stringify(this.contatos));
-  }
+  constructor(private http: HttpClient) {}
 
   obterContatos() {
-    return this.contatos;
+    return this.http.get<Contato[]>(this.API);
   }
 
   salvarContato(contato: Contato) {
-    this.contatos.push(contato);
-    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+
   }
 }

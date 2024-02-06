1af3d78607761fb592c0b5092c9923dccebe2146
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-02-06 14:56:54 -0300

    Vídeo 3.1 - obtendo dados da rota

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { Contato } from '../../componentes/contato/contato';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
   imports: [
     CommonModule,
-    ContainerComponent
+    ContainerComponent,
+    RouterLink
   ],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css'
 })
-export class PerfilContatoComponent {
+export class PerfilContatoComponent implements OnInit{
 
   contato: Contato = {
     id: 0,
@@ -22,6 +24,12 @@ export class PerfilContatoComponent {
     email: 'dev@email.com',
     aniversario: '12/10/1990',
     redes: ''
+  }
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id')
   }
 
 }
